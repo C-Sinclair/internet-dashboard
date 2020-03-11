fe5a4fe21e4af66179f34d6db9f208df412d6e45
Conor <conor@wilxite.com>
2020-03-11 21:32:10 +0000

    move to preact instead

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+const App = (
+  <main>
+    <h1>Welcome</h1>
+    <img src="../img/abstract-hexagon-geometric.jpg" />
+  </main>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { render } from "preact";
+import App from "./components/App";
+import "./styles/main";
+
+render(App, document.body);
